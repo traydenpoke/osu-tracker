a16cf76583c95fda7b70b4add3554c2ef356f8d9
Trayden <traybouc@hotmail.com>
2024-11-07 19:13:36 -0500

    format

--- client/src/components/DisplayLeaderboardUsers.tsx
+++ client/src/components/DisplayLeaderboardUsers.tsx
@@ -6,50 +6,50 @@ import '../styles/DisplayLeaderboardUsers.css';
 import Score from './Score';
 
 const DisplayLeaderboardUsers = () => {
-  const [usersScores, setUsersScores] = useState<usersScoresType>({});
+	const [usersScores, setUsersScores] = useState<usersScoresType>({});
 
-  async function getUsers(): Promise<void> {
-    console.log('fetching users');
-    // Fetch leaderboard users from the API
-    const scores = await getUsersLeaderboard('50');
-    setUsersScores(scores);
+	async function getUsers(): Promise<void> {
+		console.log('fetching users');
+		// Fetch leaderboard users from the API
+		const scores = await getUsersLeaderboard('50');
+		setUsersScores(scores);
+	}
 
-    console.log('saving scores');
-    Object.keys(usersScores).map(async (userID: string) => {
-      const scores = usersScores[userID].scores;
-      const user = {
-        id: userID,
-        username: usersScores[userID].username,
-      };
-      // const newScore = await addScores(user, scores);
-      // console.log(newScore);
-    });
-  }
+	async function saveScores(userID: string): Promise<void> {
+		const scores = usersScores[userID].scores;
+		const user = {
+			id: userID,
+			username: usersScores[userID].username,
+		};
+		const newScore = await addScores(user, scores);
+		console.log(`saved ${user.username} scores`);
+	}
 
-  useEffect(() => {
-    getUsers();
+	useEffect(() => {
+		getUsers();
 
-    const minutes = 5;
-    const interval = setInterval(() => {
-      getUsers();
-    }, minutes * 60 * 1000); // converts to ms
-    return () => clearInterval(interval);
-  }, []);
+		const minutes = 5;
+		const interval = setInterval(() => {
+			getUsers();
+		}, minutes * 60 * 1000); // converts to ms
+		return () => clearInterval(interval);
+	}, []);
 
-  return (
-    <div className='scores'>
-      {Object.keys(usersScores).map((userID: string) => (
-        <div className='userScore' key={userID}>
-          <h3>
-            <a href={`https://osu.ppy.sh/users/${userID}`} target='_blank'>
-              {usersScores[userID].username}
-            </a>
-          </h3>
-          <Score scores={usersScores[userID].scores} />
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className='scores'>
+			{Object.keys(usersScores).map((userID: string) => (
+				<div className='userScore' key={userID}>
+					<h3>
+						<a href={`https://osu.ppy.sh/users/${userID}`} target='_blank'>
+							{usersScores[userID].username}
+						</a>
+					</h3>
+					<Score scores={usersScores[userID].scores} />
+					<button onClick={() => saveScores(userID)}>Save Scores</button>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default DisplayLeaderboardUsers;
